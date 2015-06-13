0fbbb14bc07962d45ab9ad6979c78a5e9c0891ce
Kamil Pyć <kamil@jouret-mobile.be>
2015-06-13 13:46:34 +0200

    added new properties to object

--- iOS/StreetWall/StreetWall/WallDataObject.h
+++ iOS/StreetWall/StreetWall/WallDataObject.h
@@ -14,5 +14,8 @@
 
 @property (nonatomic, strong) UIImage *image;
 @property (nonatomic, assign) CLLocationCoordinate2D location;
+@property (nonatomic, strong) NSString *comment;
+@property (nonatomic, strong) NSNumber *height;
+@property (nonatomic, strong) NSNumber *width;
 
 @end
