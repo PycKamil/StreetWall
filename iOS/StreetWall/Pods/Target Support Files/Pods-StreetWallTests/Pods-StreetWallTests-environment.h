d13e1c964664f3f2f4e3a74b5166352e4cd6289f
Kamil Pyć <kamil@jouret-mobile.be>
2015-06-13 12:57:02 +0200

    added callout library

--- iOS/StreetWall/Pods/Target Support Files/Pods-StreetWallTests/Pods-StreetWallTests-environment.h
+++ iOS/StreetWall/Pods/Target Support Files/Pods-StreetWallTests/Pods-StreetWallTests-environment.h
@@ -18,6 +18,12 @@
 #define COCOAPODS_VERSION_MINOR_DVGAssetPickerController 1
 #define COCOAPODS_VERSION_PATCH_DVGAssetPickerController 0
 
+// DXCustomCallout-ObjC
+#define COCOAPODS_POD_AVAILABLE_DXCustomCallout_ObjC
+#define COCOAPODS_VERSION_MAJOR_DXCustomCallout_ObjC 0
+#define COCOAPODS_VERSION_MINOR_DXCustomCallout_ObjC 2
+#define COCOAPODS_VERSION_PATCH_DXCustomCallout_ObjC 0
+
 // MagicalRecord
 #define COCOAPODS_POD_AVAILABLE_MagicalRecord
 #define COCOAPODS_VERSION_MAJOR_MagicalRecord 2
--- iOS/StreetWall/StreetWall/MapViewController.m
+++ iOS/StreetWall/StreetWall/MapViewController.m
@@ -8,8 +8,20 @@
 
 #import "MapViewController.h"
 #import <MapKit/MapKit.h>
+#import <DXCustomCallout-ObjC/DXAnnotationView.h>
+#import <DXCustomCallout-ObjC/DXAnnotationSettings.h>
+@interface DXAnnotation : NSObject <MKAnnotation>
 
-@interface MapViewController ()
+@property(nonatomic, assign) CLLocationCoordinate2D coordinate;
+
+@end
+
+@implementation DXAnnotation
+
+
+@end
+
+@interface MapViewController () <MKMapViewDelegate>
 @property (weak, nonatomic) IBOutlet MKMapView *MapView;
 
 @end
@@ -19,6 +31,38 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    self.MapView.delegate = self;
+    [self addCallouts];
+}
+
+- (void)addCallouts {
+    DXAnnotation *annotation1 = [DXAnnotation new];
+    annotation1.coordinate = CLLocationCoordinate2DMake(12.9667, 77.5667);
+    [self.MapView addAnnotation:annotation1];
+    
+    [self.MapView setRegion:MKCoordinateRegionMakeWithDistance(annotation1.coordinate, 10000, 10000)];
+}
+
+- (MKAnnotationView *)mapView:(MKMapView *)mapView
+            viewForAnnotation:(id<MKAnnotation>)annotation {
+    
+    if ([annotation isKindOfClass:[DXAnnotation class]]) {
+        
+        UIView *pinView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"pin"]];
+        
+        UIView *calloutView = [[[NSBundle mainBundle] loadNibNamed:@"myView" owner:self options:nil] firstObject];
+        
+        DXAnnotationView *annotationView = (DXAnnotationView *)[mapView dequeueReusableAnnotationViewWithIdentifier:NSStringFromClass([DXAnnotationView class])];
+        if (!annotationView) {
+            annotationView = [[DXAnnotationView alloc] initWithAnnotation:annotation
+                                                          reuseIdentifier:NSStringFromClass([DXAnnotationView class])
+                                                                  pinView:pinView
+                                                              calloutView:calloutView
+                                                                 settings:[DXAnnotationSettings defaultSettings]];
+        }
+        return annotationView;
+    }
+    return nil;
 }
 
 - (void)didReceiveMemoryWarning {
