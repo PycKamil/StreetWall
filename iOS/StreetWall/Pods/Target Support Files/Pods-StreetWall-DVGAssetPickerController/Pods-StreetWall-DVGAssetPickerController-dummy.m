c123c97c62ab3ed7c7aedbaa06b1c44991d2e289
Kamil Pyć <kamil@jouret-mobile.be>
2015-06-13 14:17:24 +0200

    added hud

---
+++ iOS/StreetWall/Pods/Target Support Files/Pods-StreetWall-DVGAssetPickerController/Pods-StreetWall-DVGAssetPickerController-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_StreetWall_DVGAssetPickerController : NSObject
+@end
+@implementation PodsDummy_Pods_StreetWall_DVGAssetPickerController
+@end
---
+++ iOS/StreetWall/Pods/Target Support Files/Pods/Pods-environment.h
@@ -0,0 +1,86 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// AFNetworking
+#define COCOAPODS_POD_AVAILABLE_AFNetworking
+#define COCOAPODS_VERSION_MAJOR_AFNetworking 2
+#define COCOAPODS_VERSION_MINOR_AFNetworking 5
+#define COCOAPODS_VERSION_PATCH_AFNetworking 4
+
+// AFNetworking/NSURLConnection
+#define COCOAPODS_POD_AVAILABLE_AFNetworking_NSURLConnection
+#define COCOAPODS_VERSION_MAJOR_AFNetworking_NSURLConnection 2
+#define COCOAPODS_VERSION_MINOR_AFNetworking_NSURLConnection 5
+#define COCOAPODS_VERSION_PATCH_AFNetworking_NSURLConnection 4
+
+// AFNetworking/NSURLSession
+#define COCOAPODS_POD_AVAILABLE_AFNetworking_NSURLSession
+#define COCOAPODS_VERSION_MAJOR_AFNetworking_NSURLSession 2
+#define COCOAPODS_VERSION_MINOR_AFNetworking_NSURLSession 5
+#define COCOAPODS_VERSION_PATCH_AFNetworking_NSURLSession 4
+
+// AFNetworking/Reachability
+#define COCOAPODS_POD_AVAILABLE_AFNetworking_Reachability
+#define COCOAPODS_VERSION_MAJOR_AFNetworking_Reachability 2
+#define COCOAPODS_VERSION_MINOR_AFNetworking_Reachability 5
+#define COCOAPODS_VERSION_PATCH_AFNetworking_Reachability 4
+
+// AFNetworking/Security
+#define COCOAPODS_POD_AVAILABLE_AFNetworking_Security
+#define COCOAPODS_VERSION_MAJOR_AFNetworking_Security 2
+#define COCOAPODS_VERSION_MINOR_AFNetworking_Security 5
+#define COCOAPODS_VERSION_PATCH_AFNetworking_Security 4
+
+// AFNetworking/Serialization
+#define COCOAPODS_POD_AVAILABLE_AFNetworking_Serialization
+#define COCOAPODS_VERSION_MAJOR_AFNetworking_Serialization 2
+#define COCOAPODS_VERSION_MINOR_AFNetworking_Serialization 5
+#define COCOAPODS_VERSION_PATCH_AFNetworking_Serialization 4
+
+// AFNetworking/UIKit
+#define COCOAPODS_POD_AVAILABLE_AFNetworking_UIKit
+#define COCOAPODS_VERSION_MAJOR_AFNetworking_UIKit 2
+#define COCOAPODS_VERSION_MINOR_AFNetworking_UIKit 5
+#define COCOAPODS_VERSION_PATCH_AFNetworking_UIKit 4
+
+// AHEasing
+#define COCOAPODS_POD_AVAILABLE_AHEasing
+#define COCOAPODS_VERSION_MAJOR_AHEasing 1
+#define COCOAPODS_VERSION_MINOR_AHEasing 2
+#define COCOAPODS_VERSION_PATCH_AHEasing 0
+
+// DVGAssetPickerController
+#define COCOAPODS_POD_AVAILABLE_DVGAssetPickerController
+#define COCOAPODS_VERSION_MAJOR_DVGAssetPickerController 0
+#define COCOAPODS_VERSION_MINOR_DVGAssetPickerController 1
+#define COCOAPODS_VERSION_PATCH_DVGAssetPickerController 0
+
+// DXCustomCallout-ObjC
+#define COCOAPODS_POD_AVAILABLE_DXCustomCallout_ObjC
+#define COCOAPODS_VERSION_MAJOR_DXCustomCallout_ObjC 0
+#define COCOAPODS_VERSION_MINOR_DXCustomCallout_ObjC 2
+#define COCOAPODS_VERSION_PATCH_DXCustomCallout_ObjC 0
+
+// MagicalRecord
+#define COCOAPODS_POD_AVAILABLE_MagicalRecord
+#define COCOAPODS_VERSION_MAJOR_MagicalRecord 2
+#define COCOAPODS_VERSION_MINOR_MagicalRecord 3
+#define COCOAPODS_VERSION_PATCH_MagicalRecord 0
+
+// RNActivityView
+#define COCOAPODS_POD_AVAILABLE_RNActivityView
+#define COCOAPODS_VERSION_MAJOR_RNActivityView 0
+#define COCOAPODS_VERSION_MINOR_RNActivityView 0
+#define COCOAPODS_VERSION_PATCH_RNActivityView 6
+
+// TLLayoutTransitioning
+#define COCOAPODS_POD_AVAILABLE_TLLayoutTransitioning
+#define COCOAPODS_VERSION_MAJOR_TLLayoutTransitioning 1
+#define COCOAPODS_VERSION_MINOR_TLLayoutTransitioning 0
+#define COCOAPODS_VERSION_PATCH_TLLayoutTransitioning 4
+
